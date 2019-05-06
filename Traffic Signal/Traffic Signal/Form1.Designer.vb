5890b12844ca65aba18189b90ab6bdfaf0ee6c5c
Shivananda <alvashivananda11@gmail.com>
2019-05-06 14:06:43 +0530

    update

---
+++ Traffic Signal/Traffic Signal/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.OvalShape3 = New Microsoft.VisualBasic.PowerPacks.OvalShape()
+        Me.OvalShape2 = New Microsoft.VisualBasic.PowerPacks.OvalShape()
+        Me.OvalShape1 = New Microsoft.VisualBasic.PowerPacks.OvalShape()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Timer"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Panel1.Controls.Add(Me.ShapeContainer1)
+        Me.Panel1.Location = New System.Drawing.Point(95, 68)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(172, 274)
+        Me.Panel1.TabIndex = 0
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.OvalShape3, Me.OvalShape2, Me.OvalShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(172, 274)
+        Me.ShapeContainer1.TabIndex = 0
+        Me.ShapeContainer1.TabStop = False
+        '
+        'OvalShape3
+        '
+        Me.OvalShape3.FillColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.OvalShape3.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid
+        Me.OvalShape3.Location = New System.Drawing.Point(47, 198)
+        Me.OvalShape3.Name = "OvalShape3"
+        Me.OvalShape3.Size = New System.Drawing.Size(77, 66)
+        '
+        'OvalShape2
+        '
+        Me.OvalShape2.FillColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.OvalShape2.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid
+        Me.OvalShape2.Location = New System.Drawing.Point(45, 105)
+        Me.OvalShape2.Name = "OvalShape2"
+        Me.OvalShape2.Size = New System.Drawing.Size(81, 67)
+        '
+        'OvalShape1
+        '
+        Me.OvalShape1.FillColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.OvalShape1.FillStyle = Microsoft.VisualBasic.PowerPacks.FillStyle.Solid
+        Me.OvalShape1.Location = New System.Drawing.Point(45, 19)
+        Me.OvalShape1.Name = "OvalShape1"
+        Me.OvalShape1.Size = New System.Drawing.Size(80, 70)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(361, 362)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents ShapeContainer1 As PowerPacks.ShapeContainer
+    Friend WithEvents OvalShape3 As PowerPacks.OvalShape
+    Friend WithEvents OvalShape2 As PowerPacks.OvalShape
+    Friend WithEvents OvalShape1 As PowerPacks.OvalShape
+End Class
---
+++ Traffic Signal/Traffic Signal/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Dim a As Double
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label1.Text = a.ToString
+        a += 0.1
+        If a > 0.0 Then
+            OvalShape1.FillColor = Color.Red
+            OvalShape3.FillColor = Color.White
+        End If
+        If a > 1 Then
+            OvalShape2.FillColor = Color.Yellow
+            OvalShape1.FillColor = Color.White
+        End If
+        If a > 2 Then
+            OvalShape3.FillColor = Color.Green
+            OvalShape2.FillColor = Color.White
+        End If
+        If a > 3 Then
+            a = 0.0
+            Timer1.Enabled = False
+            Timer1.Enabled = True
+        End If
+    End Sub
+End Class
